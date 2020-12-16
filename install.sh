d90d19597be8d5ade3cc2ed080ea1a90cc781686
Clément Blaise <blaise_c@prodgrp.local>
2020-12-16 15:11:00 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
+checksum () {
+   curl -s https://api.github.com/repos/ca-gip/gensshconfig/releases/latest \
+  | grep browser_download_url \
+  | grep checksums \
+  | cut -d '"' -f 4 \
+  | xargs curl -sL
+}
+
 if [ "$(uname)" == "Darwin" ]; then
   echo "Downloading Darwin Release"
+  mkdir -p /var/tmp/gensshconfig
   curl -s https://api.github.com/repos/ca-gip/gensshconfig/releases/latest \
     | grep browser_download_url \
     | grep darwin_amd64 \
     | cut -d '"' -f 4 \
-    | xargs curl -sL -o /usr/local/bin/gensshconfig
+    | xargs curl -sL \
+    | tar xf - -C /var/tmp/gensshconfig/
+    sudo sh -c 'mv /var/tmp/gensshconfig/gensshconfig /usr/local/bin/ && chmod +x /usr/local/bin/gensshconfig'
+    rm -rf /var/tmp/gensshconfig
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   echo "Downloading Linux Release"
-  curl -s https://api.github.com/repos/ca-gip/gensshconfig/releases/latest \
+  mkdir -p /tmp/gensshconfig
+  curl -s  https://api.github.com/repos/ca-gip/gensshconfig/releases/latest \
     | grep browser_download_url \
     | grep linux_amd64 \
     | cut -d '"' -f 4 \
-    | xargs curl -sL -o /usr/local/bin/gensshconfig
+    | xargs curl -sL \
+    | tar xzf - -C /tmp/gensshconfig
+    sudo sh -c 'mv /tmp/gensshconfig/gensshconfig /usr/local/bin/ && chmod +x /usr/local/bin/gensshconfig'
+    rm -rf /tmp/gensshconfig
 else echo "Unsupported OS" && exit 1
 fi
 
-chmod +x /usr/local/bin/gensshconfig
 echo "Install done !"
